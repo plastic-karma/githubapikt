19d2a58b1011c9f07eb9d339f855d443faaac971
Benjamin Rogge <benni.rogge@gmail.com>
2020-01-01 11:08:24 -0800

    adding merge function for search

---
+++ src/main/kotlin/com/plastickarma/githubapikt/search/Merge.kt
@@ -0,0 +1,18 @@
+package com.plastickarma.githubapikt.search
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
+
+/**
+ * Merges given channels into one channel.
+ * @param scope Coroutine scope
+ * @param channels channels that should be merged.
+ * @return channel that contains elements from all given channels
+ */
+@ExperimentalCoroutinesApi
+fun <T> merge(scope: CoroutineScope, vararg channels: ReceiveChannel<T>) = scope.produce {
+    channels.forEach { channel -> channel.consumeEach { send(it) } }
+}
---
+++ src/test/kotlin/com/plastickarma/githubapikt/search/MergeTest.kt
@@ -0,0 +1,54 @@
+package com.plastickarma.githubapikt.search
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.channels.toList
+import kotlinx.coroutines.test.runBlockingTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+@ExperimentalCoroutinesApi
+class MergeTest {
+
+    @Test
+    fun `merge combines elements from all given channels into one`() = runBlockingTest {
+        val merged = merge(this,
+            this.produce {
+                send(1)
+                send(2)
+            },
+            this.produce {
+                send(3)
+                send(4)
+            },
+            this.produce {
+                send(5)
+                send(6)
+            }
+        ).toList()
+        assertThat(merged).containsExactly(1, 2, 3, 4, 5, 6)
+    }
+
+    @Test
+    fun `merge works with empty channels`() = runBlockingTest {
+        val merged = merge(this,
+            this.produce {
+                send(1)
+                send(2)
+            },
+            this.produce {
+            },
+            this.produce {
+                send(5)
+                send(6)
+            }
+        ).toList()
+        assertThat(merged).containsExactly(1, 2, 5, 6)
+    }
+
+    @Test
+    fun `merge works with no channels`() = runBlockingTest {
+        val merged = merge<Int>(this).toList()
+        assertThat(merged).isEmpty()
+    }
+}
