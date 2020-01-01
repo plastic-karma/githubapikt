c2fbeab718b10b24937c975550c9ff0c63627cff
Benjamin Rogge <209830+plastic-karma@users.noreply.github.com>
2019-12-31 17:33:52 -0800

    fix Timezone issue
    * better test logging
    
    * fix timezone issue in tests

--- src/test/kotlin/com/plastickarma/githubapikt/search/query/SearchQueryBuilderTest.kt
+++ src/test/kotlin/com/plastickarma/githubapikt/search/query/SearchQueryBuilderTest.kt
@@ -4,7 +4,7 @@ import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.time.Clock
 import java.time.Instant
-import java.time.ZoneId
+import java.time.ZoneOffset
 
 class SearchQueryBuilderTest {
 
@@ -55,7 +55,7 @@ class SearchQueryBuilderTest {
         val query = buildSearchQuery {
             created { last(3.months()) }
         }
-        assertThat(query).isEqualTo("created:>1969-10-02")
+        assertThat(query).isEqualTo("created:>1969-10-03")
     }
 
     @Test
@@ -63,7 +63,7 @@ class SearchQueryBuilderTest {
         val query = buildSearchQuery {
             updated { last(3.months()) }
         }
-        assertThat(query).isEqualTo("updated:>1969-10-02")
+        assertThat(query).isEqualTo("updated:>1969-10-03")
     }
 
     @Test
@@ -76,10 +76,10 @@ class SearchQueryBuilderTest {
             created { last(2.months()) }
             state = "open"
         }
-        assertThat(query).isEqualTo("label:good-first-issue label:help-wanted+language:kotlin+state:open+created:>1969-11-01+updated:>1969-10-02")
+        assertThat(query).isEqualTo("label:good-first-issue label:help-wanted+language:kotlin+state:open+created:>1969-11-02+updated:>1969-10-03")
     }
 
     private fun buildSearchQuery(builder: SearchQueryBuilder.() -> Unit): String {
-        return SearchQueryBuilder(Clock.fixed(Instant.ofEpochMilli(0), ZoneId.of("UTC"))).also(builder).build()
+        return SearchQueryBuilder(Clock.fixed(Instant.ofEpochMilli(0), ZoneOffset.UTC)).also(builder).build()
     }
 }
