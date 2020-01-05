6859331906db0c73065fc78f20ff9307e5b47bee
Benjamin Rogge <benni.rogge@gmail.com>
2020-01-05 12:16:24 -0800

    extract common function

--- src/main/kotlin/com/plastickarma/githubapikt/search/query/SearchQueryExtensions.kt
+++ src/main/kotlin/com/plastickarma/githubapikt/search/query/SearchQueryExtensions.kt
@@ -3,17 +3,21 @@ package com.plastickarma.githubapikt.search.query
 /**
  * Creates a language parameter from the given list of languages.
  */
-fun List<String>.languageParameter(): String = this.joinToString(" ") { "language:$it" }
+fun List<String>.languageParameter(): String = this.toParameter("language")
 
 /**
  * Creates a label parameter from the given list of languages.
  */
-fun List<String>.labelParameter(): String = this.joinToString(" ") { "label:$it" }
+fun List<String>.labelParameter(): String = this.toParameter("label")
 
 /**
  * Creates a date parameter from the given date builder.
  */
 fun DateBuilder?.dateParameter(prefix: String) = this?.let { "$prefix:${it.build()}" }.orEmpty()
+
+private fun List<String>.toParameter(prefix: String): String =
+    this.joinToString(" ") { "$prefix:$it" }
+
 
 /**
  * Creates a state parameter from the given state string.
